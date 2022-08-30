a851d6f8f8ac660353deabf41f864e4851ac0933
leixianming <leixianming@didiglobal.com>
2022-08-30 13:13:15 +0800

    Add license and fix comment

--- integration-test/spark3/src/test/java/org/apache/uniffle/test/GetShuffleReportForMultiPartTest.java
+++ integration-test/spark3/src/test/java/org/apache/uniffle/test/GetShuffleReportForMultiPartTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.uniffle.test;
 
 import java.io.File;
@@ -45,7 +62,7 @@ public class GetShuffleReportForMultiPartTest extends SparkIntegrationTestBase {
     dynamicConf.put(RssSparkConfig.RSS_STORAGE_TYPE.key(), StorageType.MEMORY_LOCALFILE_HDFS.name());
     addDynamicConf(coordinatorConf, dynamicConf);
     createCoordinatorServer(coordinatorConf);
-    //create multi shuffle servers
+    // Create multi shuffle servers
     createShuffleServers();
     startServers();
   }
@@ -154,7 +171,7 @@ public class GetShuffleReportForMultiPartTest extends SparkIntegrationTestBase {
       MockRssShuffleManager mockRssShuffleManager = (MockRssShuffleManager) spark.sparkContext().env().shuffleManager();
       int expectRequestNum = mockRssShuffleManager.getShuffleIdToPartitionNum().values().stream()
           .mapToInt(x -> x.get()).sum();
-      // validate getShuffleResultForMultiPart is correct before return result
+      // Validate getShuffleResultForMultiPart is correct before return result
       validateRequestCount(expectRequestNum * replicateRead);
     }
     return map;
